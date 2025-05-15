a711110b1fd1779b031d4d1521f6c1db414ea40e
ckrishnamurthy <ckrishnamurthy@mirabeltechnologies.com>
2025-05-15 21:09:02 +0530

    Update routing to use HashRouter for improved compatibility; add WelcomePage component with navigation to Site Wide Settings.

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,7 @@ import { StrictMode, Suspense, lazy } from 'react';
 import ReactDOM from 'react-dom/client';
 // import App from './App.jsx';
 import './index.css';
-import { BrowserRouter } from 'react-router-dom';
+import { HashRouter } from 'react-router-dom';
 import AppRoutes from './routers/routes.jsx';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { ErrorBoundary } from 'react-error-boundary';
@@ -28,9 +28,9 @@ ReactDOM.createRoot(document.getElementById('root')).render(
       }}
     >
       <QueryClientProvider client={queryClient}>
-        <BrowserRouter basename="/modern">
+        <HashRouter>
           <AppRoutes />
-        </BrowserRouter>
+        </HashRouter>
         {import.meta.env.DEV && (
           <Suspense fallback={null}>
             <ReactQueryDevtools initialIsOpen={false} />
--- src/routers/routeTree.js
+++ src/routers/routeTree.js
@@ -1,17 +1,19 @@
 // src/Routers/routeTree.js
 import { lazy } from 'react';
 
+const WelcomePage = lazy(() => import('../pages/Welcome'));
 const SiteWiseSettingsPage = lazy(() => import('../pages/SiteWiseSettingsPage'));
 
 export const routes = [
   {
+    path: '/',
+    component: WelcomePage,
+    children: [],
+    exact: true,
+  },
+  {
     path: '/sitewidesettings',
     component: SiteWiseSettingsPage,
-    children: [
-      {
-        index: true,
-        component: SiteWiseSettingsPage,
-      },
-    ],
-  },
+    exact: true,
+  }
 ];
