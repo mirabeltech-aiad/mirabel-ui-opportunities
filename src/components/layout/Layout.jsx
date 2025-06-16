40182cfad50cd8d25ccff348ad345938630913ce
ckrishnamurthy <ckrishnamurthy@mirabeltechnologies.com>
2025-06-16 14:06:42 +0530

    DEV-31820 Remove Header component from Layout for cleaner structure and improved modularity.

--- src/components/layout/Layout.jsx
+++ src/components/layout/Layout.jsx
@@ -3,7 +3,7 @@ import { Header } from "./Header";
 export function Layout({ children }) {
   return (
     <div className="min-h-screen bg-background">
-      <Header />
+      {/* <Header /> */}
         {children}
     </div>
   );
