aaf3c5b4f99b90f10c724890796d65a0634ffeff
Subbu Aakula <avsubbarayudu@mirabeltechnologies.com>
2025-07-12 00:03:19 +0530

    DEV-33170 Test

--- src/features/Homepage/index.jsx
+++ src/features/Homepage/index.jsx
@@ -2,9 +2,13 @@ import React from 'react';
 import { HomeProvider } from './contexts/HomeContext';
 import TabNavigation from './components/TabNavigation';
 import HelpSystem from './components/HelpSystem';
-import { Toaster } from '@/components/ui/toaster';
+import navigationService from './services/navigationService';
 
 const Home = () => {
+  React.useEffect(() => {
+    // Load session details and set in localStorage on /ui60/home load
+    navigationService.loadSessionDetails();
+  }, []);
   return (
     <HomeProvider>
       <div className="min-h-screen bg-gray-50">
