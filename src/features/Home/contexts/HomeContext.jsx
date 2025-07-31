39d516ca4b2dcc626b0c00de27e1f24ea11f244f
Subbu Aakula <avsubbarayudu@mirabeltechnologies.com>
2025-07-31 21:18:11 +0530

    DEV-33170 Test dashboard

--- src/features/Home/contexts/HomeContext.jsx
+++ src/features/Home/contexts/HomeContext.jsx
@@ -32,14 +32,14 @@ const ACTIONS = {
 // Initial state
 const initialState = {
   tabs: [
-    {
-      id: 'dashboard',
-      title: 'Sales Dashboard',
-      component: 'Dashboard',
-      type: 'component',
-      closable: false,
-      icon: '' // Optionally add an icon for the dashboard tab
-    },
+    // {
+    //   id: 'dashboard',
+    //   title: 'Sales Dashboard',
+    //   component: 'Dashboard',
+    //   type: 'component',
+    //   closable: false,
+    //   icon: '' // Optionally add an icon for the dashboard tab
+    // },
     {
       id: 'inbox',
       title: 'Inbox',
