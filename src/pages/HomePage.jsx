08c9a0df4a7a0c353ad33cc73f2406107500b542
Subbu Aakula <avsubbarayudu@mirabeltechnologies.com>
2025-08-04 16:07:24 +0530

    DEV-33170 removed commented code

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -9,19 +9,6 @@ import { getTopPath } from '../utils/commonHelpers';
  * that serve as entry points to feature modules
  */
 const HomePage = () => {
-  React.useEffect(() => {
-    //Dynamically create and bind a script tag
-    const script = document.createElement('script');
-    // script.src = `${getTopPath()}/intranet/localizer.js.axd?v=5.24.2`; // <-- Replace with actual script path
-    // script.async = true;
-    // document.body.appendChild(script);
-
-   
-    // Optional: Clean up the script tag on unmount
-    // return () => {
-    //   document.body.removeChild(script);
-    // };
-  }, []);
   return <Home />;
 };
 
